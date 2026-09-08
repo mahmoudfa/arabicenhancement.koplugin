30c52f545a162a96c3378257cb438571223ebfea
Mahmoud Fahmy <45940453+mahmoudfa@users.noreply.github.com>
2026-09-09 00:44:13 +0300

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,9 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "arabic_enhancement",
-    fullname = _("Arabic Enhancement"),
-    description = _([[Automatically detects Arabic-language books and applies right-to-left page turning, right text alignment, and a fallback Arabic font.]]),
-    version = "1.0.0",
-    author = "Mahmoud",
-}
