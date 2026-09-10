29ee972f9c7316fec4cedbe6f33dba4da834ab96
Mahmoud Fahmy <45940453+mahmoudfa@users.noreply.github.com>
2026-09-10 20:25:28 +0300

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,8 +0,0 @@
-local _ = require("gettext")
-
-return {
-    name = "arabicenhancement",
-    fullname = _("Arabic Enhancement"),
-    description = _([[Detects Arabic-language books and automatically switches page-turn direction to right-to-left, right-aligns text, and applies a fallback font — without overwriting settings you've already configured.]]),
-    version = 1,
-}
